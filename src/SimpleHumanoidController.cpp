7054ae997bf977223f050ec32b4da06d623720a6
Khang Pham <phamthekhang632@gmail.com>
2025-08-17 20:50:12 +0200

    Update cpp and header - ensure working directory

--- src/SimpleHumanoidController.cpp
+++ src/SimpleHumanoidController.cpp
@@ -8,17 +8,40 @@ SimpleHumanoidController::SimpleHumanoidController(mc_rbdyn::RobotModulePtr rm, 
   solver().addTask(postureTask);
   solver().setContacts({{}});
 
+  jointIndex = robot().jointIndexByName("NECK_Y");
+
   mc_rtc::log::success("SimpleHumanoidController init done ");
 }
 
 bool SimpleHumanoidController::run()
 {
+  if(std::abs(postureTask->posture()[jointIndex][0] - robot().mbc().q[jointIndex][0]) < 0.05)
+  {
+    switch_target();
+  }
   return mc_control::MCController::run();
 }
 
 void SimpleHumanoidController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
+}
+
+void SimpleHumanoidController::switch_target()
+{
+  std::map<std::string, std::vector<double>> targetPosture;
+
+  if(goingLeft)
+  {
+      targetPosture["NECK_Y"] = {0.5};  // radians, looking left
+  }
+  else
+  {
+      targetPosture["NECK_Y"] = {-0.5};
+  }
+  postureTask->target(targetPosture);
+
+  goingLeft = !goingLeft;
 }
 
 CONTROLLER_CONSTRUCTOR("SimpleHumanoidController", SimpleHumanoidController)
--- src/SimpleHumanoidController.h
+++ src/SimpleHumanoidController.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <mc_control/mc_controller.h>
-
-
 #include "api.h"
 
 struct SimpleHumanoidController_DLLAPI SimpleHumanoidController : public mc_control::MCController
@@ -10,6 +8,11 @@ struct SimpleHumanoidController_DLLAPI SimpleHumanoidController : public mc_cont
   SimpleHumanoidController(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
 
   bool run() override;
-
   void reset(const mc_control::ControllerResetData & reset_data) override;
+  void switch_target();
+
+private:
+    mc_rtc::Configuration config_;
+    int jointIndex = 0;
+    bool goingLeft = true;
 };
