f9d5d727c65929add373850d821f7ced7f90a256
Khang Pham <phamthekhang632@gmail.com>
2025-09-10 16:34:14 +0200

    Update state SimpleHumanoidController_Initial - adding button to start movement

--- src/states/SimpleHumanoidController_Initial.cpp
+++ src/states/SimpleHumanoidController_Initial.cpp
@@ -1,5 +1,6 @@
-#include "SimpleHumanoidController_Initial.h"
+#include <mc_control/fsm/Controller.h>
 
+#include "SimpleHumanoidController_Initial.h"
 #include "../SimpleHumanoidController.h"
 
 void SimpleHumanoidController_Initial::configure(const mc_rtc::Configuration &config)
@@ -8,19 +9,31 @@ void SimpleHumanoidController_Initial::configure(const mc_rtc::Configuration &co
 
 void SimpleHumanoidController_Initial::start(mc_control::fsm::Controller &ctl_)
 {
-  auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
+  // auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
+  ctl_.gui()->addElement(
+      {},
+      mc_rtc::gui::Button("Start Moving", [this]()
+                          { startMoving_ = true; }));
 }
 
 bool SimpleHumanoidController_Initial::run(mc_control::fsm::Controller &ctl_)
 {
-  auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
-  output("OK");
-  return true;
+  // auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
+  // output("OK");
+  // return true;
+
+  if (startMoving_)
+  {
+    output("Start Moving");
+    return true;
+  }
+  return false;
 }
 
 void SimpleHumanoidController_Initial::teardown(mc_control::fsm::Controller &ctl_)
 {
-  auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
+  // auto &ctl = static_cast<SimpleHumanoidController &>(ctl_);
+  ctl_.gui()->removeElement({}, "Start Moving");
 }
 
 EXPORT_SINGLE_STATE("SimpleHumanoidController_Initial", SimpleHumanoidController_Initial);
--- src/states/SimpleHumanoidController_Initial.h
+++ src/states/SimpleHumanoidController_Initial.h
@@ -5,10 +5,10 @@
 struct SimpleHumanoidController_Initial : mc_control::fsm::State
 {
   void configure(const mc_rtc::Configuration &config) override;
-
   void start(mc_control::fsm::Controller &ctl) override;
-
   bool run(mc_control::fsm::Controller &ctl) override;
-
   void teardown(mc_control::fsm::Controller &ctl) override;
+
+private:
+  bool startMoving_ = false;
 };
